992ad680ad4ff19ab9264ca0467a36d60ddddf71
TwiZxD <iamtwiz@gmail.com>
2018-07-05 12:37:44 +0200

    Added Solution for problem 9.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         long timerStart = System.currentTimeMillis();
 
-	    new Problem7();
+	    new Problem9();
 
 	    System.out.println("Found in: "+ (System.currentTimeMillis() - timerStart) + " ms");
     }
