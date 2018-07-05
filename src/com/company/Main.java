4a16309165a083812498f3d0e6a2ce3a3c48f050
TwiZxD <iamtwiz@gmail.com>
2018-07-05 12:07:22 +0200

    Added Solution for problem 5, 6 and 7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,11 @@ username: twizeuler
 */
 public class Main {
     public static void main(String[] args) {
-	    new Problem4();
+        long timerStart = System.currentTimeMillis();
+
+	    new Problem7();
+
+	    System.out.println("Found in: "+ (System.currentTimeMillis() - timerStart) + " ms");
     }
 }
 
